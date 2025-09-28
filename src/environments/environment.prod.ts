adaf21cf9b4126a77df72e4be910bac6e980dfe0
Surendra2284 <92510811+Surendra2284@users.noreply.github.com>
2025-09-29 01:22:56 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend1-m4j8.onrender.com/' // 🔁 Replace with your actual backend URL
+  apiUrl: 'https://backend1-m4j8.onrender.com' // 🔁 Replace with your actual backend URL
 };
